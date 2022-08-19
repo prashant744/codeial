e74f64582fbb6929befa3be7890bc954bb8d6895
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-20 00:48:05 +0530

    add posts path

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return <div className="App">App</div>;
+import { fetchPosts } from '../actions/posts';
+import { PostsList } from './';
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <PostsList posts={posts} />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+  };
+}
+export default connect(mapStateToProps)(App);
