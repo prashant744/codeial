02dee7d94867e1623c249e42f2ccfb4f2dacf47c
Prashant Dhote <prashantdhote400@gmail.com>
2022-08-21 19:08:13 +0530

    update login form

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { PostsList, FriendsList } from './';
+
+class Home extends Component {
+  render() {
+    const { posts, friends, isLoggedin } = this.props;
+    return (
+      <div className="home">
+        <PostsList posts={posts} />
+        {isLoggedin && <FriendsList friends={friends} />}
+      </div>
+    );
+  }
+}
+
+export default Home;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,24 @@
 import PostsList from './PostsList';
-import App from './App';
+// import App from './App';
+import Navbar from './Navbar';
+import Home from './Home';
+import Page404 from './Page404';
+import Login from './Login';
+import Signup from './Signup';
+import Settings from './Settings';
+import UserProfile from './UserProfile';
+import FriendsList from './FriendsList';
+import FriendsListItem from './FriendsListItem';
 
-export { PostsList, App };
+export {
+  PostsList,
+  Navbar,
+  Home,
+  Page404,
+  Login,
+  Signup,
+  Settings,
+  UserProfile,
+  FriendsList,
+  FriendsListItem,
+};
